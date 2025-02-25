5ff0a7d7d9c71e1ca23ca99652b9bb94b0b96b25
Andy Kluger <AndyKluger@gmail.com>
2025-02-25 02:50:25 -0500

    Replace minigalaxy and lib32s with heroic (and wine)

--- 40-desktop-gaming.sh
+++ 40-desktop-gaming.sh
@@ -1,15 +1,10 @@
 AddPackage steam # Valve's digital software delivery system
   AddPackage ttf-liberation # Font family which aims at metric compatibility with Arial, Times New Roman, and Courier New
 
+AddPackage --foreign heroic-games-launcher # Native GOG, Epic Games and Amazon games launcher for Linux
 AddPackage --foreign parallel-launcher # Modern N64 Emulator
 AddPackage --foreign wiiudownloader # Allows to download encrypted wiiu files from nintendo's official servers
 AddPackage --foreign xemu-bin # Original Xbox Emulator
-
-# -- Oddworld NnT --
-AddPackage --foreign minigalaxy # A simple GOG client for Linux
-AddPackage lib32-glu # Mesa OpenGL utility library (32 bits)
-AddPackage lib32-libxcursor # X cursor management library (32-bit)
-AddPackage lib32-sdl2-compat # An SDL2 compatibility layer that uses SDL3 behind the scenes - 32-bit
 
 # -- via flatpak --
 # cemu (or maybe use cemu-bin?)
