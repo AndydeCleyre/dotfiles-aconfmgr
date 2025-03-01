5c7e5f662a8f3f5a62c75b7f3b6b7a67a52594dc
Andy Kluger <AndyKluger@gmail.com>
2025-03-01 01:47:19 -0500

    Replace cemu flatpak with AUR pkg

--- 40-desktop-gaming.sh
+++ 40-desktop-gaming.sh
@@ -1,10 +1,8 @@
 AddPackage steam # Valve's digital software delivery system
   AddPackage ttf-liberation # Font family which aims at metric compatibility with Arial, Times New Roman, and Courier New
 
+AddPackage --foreign cemu # Software to emulate Wii U games and applications on PC
+  AddPackage --foreign wiiudownloader # Allows to download encrypted wiiu files from nintendo's official servers
 AddPackage --foreign heroic-games-launcher # Native GOG, Epic Games and Amazon games launcher for Linux
 AddPackage --foreign parallel-launcher # Modern N64 Emulator
-AddPackage --foreign wiiudownloader # Allows to download encrypted wiiu files from nintendo's official servers
 AddPackage --foreign xemu-bin # Original Xbox Emulator
-
-# -- via flatpak --
-# cemu (or maybe use cemu-bin?)
